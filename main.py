acfc7627e81a87578e6cf1ef09304ec7630b6d7c
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-04 11:02:37 -0800

    Changes to caching system

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ with open("derpikey.txt") as m:
 
 # Our empty search cache variable
 # The format is ["search string", {"tags":"image tags", "image":"image url"}, {"tags":"image tags", "image":"image url"}, ...]
-SEARCH_CACHE = []
+SEARCH_CACHE = {}
+IMAGE_CACHE = {}
 # A tiny function to get the current time in milliseconds
-current_milli_time = lambda: int(round(time.time() * 1000))
+current_milli_time = lambda: round(time.time() * 1000, 3)
 
 @client.event
 async def on_ready():
@@ -33,6 +34,39 @@ async def on_message(message):
     if message.content.startswith('!privacy'):
         await client.send_message(message.channel, '**DerpiBot Privacy Policy**\n1. DerpiBot will anonymously track search queries for the purpose of determining popular searches and images to cache in RAM. Never is your user ID, name, etc. stored in this process.\n2. DerpiBot will never store any user identifiers (such as usernames, user ids, etc) unless absolutely required, such as for user preset searches.')
 
+    if message.content == "!caches":
+        embed = discord.Embed(color=discord.Color.red())
+        embed.add_field(name="[RAM] Image Cache", value=str(len(IMAGE_CACHE)))
+        embed.add_field(name="[RAM] Search Cache", value=str(len(SEARCH_CACHE)))
+        await client.send_message(message.channel, embed=embed)
+
+    if message.content == "!bench":
+        average_time = 0
+        test = "!search pt,safe,solo"
+        for i in range(1000):
+            # Start search timer
+            search_start = current_milli_time()
+            # Extract search query
+            search_string = test.split(" ",1)[1].replace(" ", "+")
+            # Set cache level to -1 for unknown
+            cache_level = -1
+            content = None
+
+            # Search our RAM cache first
+            if search_string in SEARCH_CACHE:
+                cache_level = 0
+                random_image_id = SEARCH_CACHE[search_string][randint(0,len(SEARCH_CACHE[search_string])-1)]
+                random_entry = IMAGE_CACHE[random_image_id]
+
+            if cache_level == -1:
+                pass
+
+            search_end = current_milli_time()
+
+            average_time += search_end-search_start
+
+        await client.send_message(message.channel, str((average_time/1000)*1000) + "μs")
+
     # Search command
     if message.content.startswith('!search '):
         # Start search timer
@@ -44,11 +78,10 @@ async def on_message(message):
         content = None
 
         # Search our RAM cache first
-        for entry in SEARCH_CACHE:
-            if entry["search_string"] == search_string:
-                # If the search term is found in the cache, set cache level to 0 (completely RAM based) and set our entry to a random one
-                cache_level = 0
-                content = entry
+        if search_string in SEARCH_CACHE:
+            cache_level = 0
+            random_image_id = SEARCH_CACHE[search_string][randint(0,len(SEARCH_CACHE[search_string])-1)]
+            random_entry = IMAGE_CACHE[random_image_id]
         
         if cache_level == -1:
             # Build and get search from Derpibooru's API
@@ -57,18 +90,27 @@ async def on_message(message):
             content = r.json()
             entry_count = len(content["search"])
 
+            # If our array of images is empty, Derpibooru didn't send us any results. Error out.
+            if len(content["search"]) == 0:
+                embed = discord.Embed(color=discord.Color.red())
+                embed.add_field(name="Error!", value="No results were returned.")
+                await client.send_message(message.channel, embed=embed)
+                return
+
             # Add this search to the RAM cache for later use
-            temp_search_cache = {"search_string":search_string, "search":[]}
+            temp_search_cache = []
             for entry in content["search"]:
-                t = {"tags":entry["tags"], "image":entry["image"]}
-                temp_search_cache["search"].append(t)
-            SEARCH_CACHE.append(temp_search_cache)
+                temp_search_cache.append(int(entry["id"]))
+                if entry["id"] not in IMAGE_CACHE:
+                    t = {"tags":entry["tags"], "image":entry["image"]}
+                    IMAGE_CACHE[int(entry["id"])] = t
+            SEARCH_CACHE[search_string] = temp_search_cache
 
             # Set cache level to 2, for Derpibooru Server
             cache_level = 2
 
-        # Select a random entry from the query to be our lucky image
-        random_entry = content["search"][randint(0,len(content["search"])-1)]
+            # Select a random entry from the query to be our lucky image
+            random_entry = content["search"][randint(0,len(content["search"])-1)]
 
         # Done searching, end the timer
         search_end = current_milli_time()
@@ -83,7 +125,11 @@ async def on_message(message):
             embed.add_field(name="Search Level", value="RAM Search Cache [FAST]")
         else:
             embed.add_field(name="Search Level", value="Unknown")
-        embed.add_field(name="Search Query Time", value=str(search_end-search_start) + "ms")
+        search_time = search_end-search_start
+        if search_time < 0.9:
+            embed.add_field(name="Search Query Time", value=str(search_time*1000) + "μs")
+        else:
+            embed.add_field(name="Search Query Time", value=str(search_time) + "ms")
         embed.set_image(url="https:"+random_entry["image"])
 
         # Send our embed
